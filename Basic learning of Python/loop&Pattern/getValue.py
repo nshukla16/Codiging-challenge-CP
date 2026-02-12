c629d4775feef344af273c4e6600e8467ac87db8
nshukla16 <neerajshuklamrt@gmail.com>
2026-02-12 12:49:59 +0000

    print value 1 to n asc order

--- Basic learning of Python/loop&Pattern/getValue.py
+++ Basic learning of Python/loop&Pattern/getValue.py
@@ -1,9 +1,18 @@
+#print n times given value
 def printf(i,n):
     if(i>n):
         return
     print('rahul')
     # i+=1
     printf(i+1,n)
-printf(1,5)
+# printf(1,5)
 
+#print value from 1 to n asc order
+def printNumAsc(i,n):
+    if(i>n):
+        return
+    print(i)
+
+    printNumAsc(i+1,n)
+printNumAsc(1,5)
 
